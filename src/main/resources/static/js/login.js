79f7c9c781e1999b949817192b877bb3252a7a56
puyiliang <13641378429@163.com>
2019-08-26 18:03:29 +0800

    登录页面 前端页面的编写

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,28 @@
+//切换注册
+$(".register-link").on("click",function(){
+    $("#loginBox").hide("slow");
+    $("#registerBox").show("slow");
+});
+//切换登录
+$(".login-link").on("click",function(){
+    $("#registerBox").hide("slow");
+    $("#loginBox").show("slow");
+
+});
+//切换忘记密码
+$(".forgot-link").on("click",function(){
+    $("#submitButton").text("找回");
+    $("#loginTitleBox").text("找回密码");
+    $("#passwordBox").hide();
+    $("#showLoginButton").show();
+
+});
+//返回登录
+$(".login-back-link").on("click",function(){
+    $("#submitButton").text("登录");
+    $("#loginTitleBox").text("登录");
+    $("#passwordBox").show();
+    $("#showLoginButton").hide();
+
+});
+
