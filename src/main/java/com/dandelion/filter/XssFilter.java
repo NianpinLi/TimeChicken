f3f51b033956012d3a5686601337267b0ff88534
puyiliang <13641378429@163.com>
2019-10-15 14:34:43 +0800

    框架新增Xss攻击过滤。

---
+++ src/main/java/com/dandelion/filter/XssFilter.java
@@ -0,0 +1,79 @@
+package com.dandelion.filter;
+
+import com.dandelion.utils.ObjectUtil;
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * ClassName: XssFilter
+ * date:      2019/10/15 14:21
+ * author:    puyiliang
+ * description: Xss 攻击过滤器
+ */
+@Slf4j
+public class XssFilter implements Filter{
+    /**
+     * 是否过滤富文本内容
+     */
+    private static boolean IS_INCLUDE_RICH_TEXT = false;
+
+    public List<String> excludes = Lists.newArrayList();
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException,ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        if(handleExcludeURL(req, resp)){
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        XssHttpServletRequestWrapper xssRequest = new XssHttpServletRequestWrapper((HttpServletRequest) request,IS_INCLUDE_RICH_TEXT);
+        filterChain.doFilter(xssRequest, response);
+    }
+
+    private boolean handleExcludeURL(HttpServletRequest request, HttpServletResponse response) {
+
+        if (excludes == null || excludes.isEmpty()) {
+            return false;
+        }
+
+        String url = request.getServletPath();
+        for (String pattern : excludes) {
+            Pattern p = Pattern.compile("^" + pattern);
+            Matcher m = p.matcher(url);
+            if (m.find()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        String isIncludeRichText = filterConfig.getInitParameter("isIncludeRichText");
+        if(!ObjectUtil.isNull(isIncludeRichText)){
+            IS_INCLUDE_RICH_TEXT = Boolean.parseBoolean(isIncludeRichText);
+        }
+        String temp = filterConfig.getInitParameter("excludes");
+        if (temp != null) {
+            String[] url = temp.split(",");
+            for (int i = 0; url != null && i < url.length; i++) {
+                excludes.add(url[i]);
+            }
+        }
+    }
+
+    @Override
+    public void destroy() {}
+
+}
--- src/main/java/com/dandelion/utils/StringUtil.java
+++ src/main/java/com/dandelion/utils/StringUtil.java
@@ -87,61 +87,6 @@ public class StringUtil {
     }
 
     /**
-     * 字符串MD5加密，加盐
-     * @param str String
-     * @return String
-     */
-    public static String getMD5(String str) throws Exception{
-        Random r = new Random();
-        StringBuilder sb = new StringBuilder(16);
-        sb.append(r.nextInt(99999999)).append(r.nextInt(99999999));
-        int len = sb.length();
-        if (len < 16) {
-            for (int i = 0; i < 16 - len; i++) {
-                sb.append("0");
-            }
-        }
-        String salt = sb.toString();
-        str = md5Hex(str + salt);
-        char[] cs = new char[48];
-        for (int i = 0; i < 48; i += 3) {
-            cs[i] = str.charAt(i / 3 * 2);
-            char c = salt.charAt(i / 3);
-            cs[i + 1] = c;
-            cs[i + 2] = str.charAt(i / 3 * 2 + 1);
-        }
-        return new String(cs);
-    }
-
-    /**
-     * 获取十六进制字符串形式的MD5摘要
-     * @param str String
-     * @return String
-     */
-    public static String md5Hex(String str) throws Exception{
-        MessageDigest md5 = MessageDigest.getInstance("MD5");
-        byte[] bs = md5.digest(str.getBytes());
-        return new String(new Hex().encode(bs));
-    }
-
-    /**
-     * 校验String 和 加密之后的 md5 是否相同
-     * @param str String
-     * @return String
-     */
-    public static boolean verify(String str, String md5) throws Exception{
-        char[] cs1 = new char[32];
-        char[] cs2 = new char[16];
-        for (int i = 0; i < 48; i += 3) {
-            cs1[i / 3 * 2] = md5.charAt(i);
-            cs1[i / 3 * 2 + 1] = md5.charAt(i + 2);
-            cs2[i / 3] = md5.charAt(i + 1);
-        }
-        String salt = new String(cs2);
-        return md5Hex(str + salt).equals(new String(cs1));
-    }
-
-    /**
      * Json to bean
      * @param value String
      * @param clazz Class
