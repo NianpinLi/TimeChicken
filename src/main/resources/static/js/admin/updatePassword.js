e7b7d1e15c235f94ed58150d0785a362ff3048ca
puyiliang <13641378429@163.com>
2019-12-16 14:16:17 +0800

    index页面功能

---
+++ src/main/resources/static/js/admin/updatePassword.js
@@ -0,0 +1,18 @@
+layui.use(['form'], function () {
+    var form = layui.form
+        , $ = layui.jquery;
+
+    //监听提交
+    form.on('submit(submit)', function (data) {
+        $.post(
+            "/admin/savePassword",
+            data.field,
+            function(obj){
+                //回调函数
+                responseObj(obj);
+            },
+            "JSON"
+        );
+        return false;
+    });
+});
