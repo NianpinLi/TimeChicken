540858e46bd77b5e74ee9199ab3aa82d2f74bc43
puyiliang <13641378429@163.com>
2019-12-17 11:36:25 +0800

    提交

--- src/main/java/com/dandelion/utils/PictureVerificationCodeUtil.java
+++ src/main/java/com/dandelion/utils/PictureVerificationCodeUtil.java
@@ -3,8 +3,6 @@ package com.dandelion.utils;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Random;
